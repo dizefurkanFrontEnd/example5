6196ae3c2e72560aa795adf351fb5f2efe051413
Furkan Dize <dizefurkan@gmail.com>
2018-01-20 11:24:26 +0300

    Added header scroll effect

--- _assets/js/index.js
+++ _assets/js/index.js
@@ -16,3 +16,28 @@ $(document).ready(function() {
   //     $('header, section, .box').css("display","block");
   // }, 1000);
 });
+
+$(window).ready(function() {
+  var lastScrollTop = 0;
+  $(window).on("scroll", function() {
+    var scrollCurrent = $(window).scrollTop();
+    var scrollHeight = $(document).height();
+    var scrollWidth = $(document).width();
+    var scrollPosition = $(window).height() + $(window).scrollTop();
+
+    if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+      // when scroll to bottom of the page
+      $("#shortcutButton").css("display", "block");
+    }
+    
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+      // downscroll code
+      $("header").addClass("off");
+    } else {
+      // upscroll code
+      $("header").removeClass("off");
+    }
+    lastScrollTop = st;
+  });
+});
